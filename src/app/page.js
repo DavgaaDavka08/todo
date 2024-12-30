42b02a5b90b9faaaf90e8ce7192fd7142103eedb
24LP1757 24LP1757 <24LP1757@G304-CC0217.local>
2024-12-30 14:25:32 +0800

    todo

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,52 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import { useState } from "react";
+import style from "./page.module.css";
 
 export default function Home() {
+  const [printle, setPrintle] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+  const clickHandler = () => {
+    setPrintle([...printle, newTodo]);
+  };
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    <div className={style.maidev}>
+      <div className={style[`todo-container`]}>
+        <h1 className={style.mainText}>To-Do list</h1>
+        <div className={style.flex}>
+          <input
+            className={style.mainInput}
+            type="text"
+            placeholder="Add a new task"
+            onChange={(e) => setNewTodo(e.target.value)}
+          />
+          <button className={style.mainButton} onClick={clickHandler}>
+            Add
+          </button>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className={style.flex2}>
+          <button className={style.containerButton1}>All</button>
+          <button className={style.containerButton2}>Active</button>
+          <button className={style.containerButton3}>Completed</button>
+        </div>
+        <div>
+          <p className={style.undertext}>No tasks yet. Add one above!</p>
+        </div>
+        {printle.map((todo) => {
+          return (
+            <div className={style.checkBox}>
+              <div className={style.flex3}>
+                <input className={style.box} type="checkBox" name="" id="" />
+                <p>{todo}</p>
+              </div>
+              <button>Delete</button>
+            </div>
+          );
+        })}
+        <div className={style.footerflex}>
+          <p className={style.undertext2}>Powered by</p>
+          <p className={style.undertext3}>Pinecone academy</p>
+        </div>
+      </div>
     </div>
   );
 }
